e90e0c2019157c11211bc391be8f7d81714a1b5e
Tamás Sulyok <tamas.sulyok@stud.uni-corvinus.hu>
2021-10-28 12:28:15 +0200

    a

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
+using week06.MnbServiceReference;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            BindingList<RateData> Rates = new BindingList<RateData>();
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void Fuggveny()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
+
 }
