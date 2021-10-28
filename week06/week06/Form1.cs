3524167afbdcdc6c137e6c1704efa6462af62b4d
Tamás Sulyok <tamas.sulyok@stud.uni-corvinus.hu>
2021-10-28 12:56:20 +0200

    c

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06.Entities;
 using week06.MnbServiceReference;
@@ -21,15 +22,23 @@ namespace week06
         public Form1()
         {
             InitializeComponent();
-            
+            //GetCurrencies();
+            Fuggveny();
+            Diagram();
             dataGridView1.DataSource = Rates;
+            BindingList<String> Currencies = new BindingList<string>();
+            comboBox1.DataSource = Currencies;
         }
 
         private void Fuggveny()
         {
+            Rates.Clear();
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
+                //currencyNames = comboBox1.SelectedItem.ToString(),
+                //startDate = dateTimePicker1.Value.ToString(),
+                //endDate = dateTimePicker2.Value.ToString(),
                 currencyNames = "EUR",
                 startDate = "2020-01-01",
                 endDate = "2020-06-30"
@@ -54,7 +63,45 @@ namespace week06
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+
+            
         }
+        private void Diagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            Fuggveny();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            Fuggveny();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Fuggveny();
+        }
+
+        
     }
+
 
 }
